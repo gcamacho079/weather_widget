c2863ba5488b9f2e17f641bd2c28eba32971a317
Lupe <lupe@acoorg.com>
2017-11-28 08:47:00 -0800

    Add callForecast function for gathering 5 day weather data

--- js/script.js
+++ js/script.js
@@ -1,31 +1,42 @@
 const weatherAlert = {
 
 		onReady: function() {
-			weatherAlert.getWeather();
+			const zipCode = 97266;
+			weatherAlert.callWeather(zipCode);
+			weatherAlert.callForecast(zipCode);
 		},
 
-		getWeather: function() {
-      const zipCode = 97266;
-			const apiURL = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "&units=imperial&APPID=5d9bc196ef530b2e4b0ed9abf5c104eb";
+		callWeather: function(zip) {
+			const currentWeatherURL = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&units=imperial&APPID=5d9bc196ef530b2e4b0ed9abf5c104eb";
 			$.ajax({
 				dataType: 'json',
-				url: apiURL
+				url: currentWeatherURL
 			}).done(function(data) {
-				weatherAlert.constructDOM(data);
-
+				weatherAlert.gatherWeather(data);
 			}).fail(function(jqXHR, textStatus, errorThrown) {
 				console.log(textStatus + ": " + errorThrown);
 			});
-
 		},
 
-		constructDOM: function(json) {
+		gatherWeather: function(json) {
 			const weatherIcon = "https://openweathermap.org/img/w/" + json.weather[0].icon + ".png";
 			const cityName = json.name;
 			const mainWeather = json.weather[0].main;
 			const weatherDescription = json.weather[0].description;
 			const temperature = json.main.temp;
 			weatherAlert.populateDOM(weatherIcon, cityName, mainWeather, weatherDescription, temperature);
+		},
+
+		callForecast: function(zip) {
+			const fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?zip=" + zip + "&units=imperial&APPID=5d9bc196ef530b2e4b0ed9abf5c104eb";
+			$.ajax ({
+				dataType: 'json',
+				url: fiveDayURL
+			}).done(function(data) {
+				console.log(data);
+			}).fail(function(jqXHR, textStatus, errorThrown) {
+				console.log(textStatus + ": " + errorThrown);
+			});
 		},
 
 		populateDOM: function(icon, city, weather, description, temp) {
