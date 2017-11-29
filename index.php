7426f9a8bd978dcccc88043d0302b4697bb05302
Lupe <lupe@acoorg.com>
2017-11-29 10:56:35 -0800

    Create call to wunderground weather alert

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles/style.css">
 
 </head>
 <body>
@@ -14,9 +15,15 @@
   <div class="container">
 
     <div class="table-responsive">
-      <table class="table">
+      <table class="table" id="forecast-table">
         <tr id="forecast__days">
           <!--Insert Days from API Call-->
+        </tr>
+        <tr id="forecast__temps">
+        </tr>
+        <tr id="forecast__icon">
+        </tr>
+        <tr id="forecast__conditions">
         </tr>
       </table>
 
--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 const weatherApp = {
 
 		onReady: function() {
-			const zipCode = 97266;
+			const zipCode = 99827;
 			weatherApp.callForecast(zipCode);
+			weatherApp.callAlerts(zipCode);
 		},
+
+		/*********
+		 API CALLS
+		 *********/
 
 		callForecast: function(zip) {
 			const wundergroundURL = "http://api.wunderground.com/api/ab855c8f628983eb/forecast10day/q/" + zip + ".json"
@@ -11,30 +16,62 @@ const weatherApp = {
 				dataType: 'json',
 				url: wundergroundURL
 			}).done(function(data) {
-				console.log(data);
-				weatherApp.createDataArrays(data.forecast.simpleforecast.forecastday);
+				weatherApp.createForecastArrays(data.forecast.simpleforecast.forecastday);
 			}).fail(function(jqXHR, textStatus, errorThrown) {
 				console.log(textStatus + ": " + errorThrown);
 			});
 		},
 
-		createDataArrays: function(json) {
-			const lengthOfForecast = 5;
-			console.log(json);
-			let forecastDays = [];
-			let date = "";
-			for (i = 0; i < lengthOfForecast; i++) {
-				date = json[i].date.weekday_short + " " + json[i].date.month + "/" + json[i].date.day;
-				forecastDays.push(date);
-			}
-			weatherApp.buildForecastTable(forecastDays);
-			console.log(forecastDays);
+		callAlerts: function(zip) {
+			const alertURL = "http://api.wunderground.com/api/ab855c8f628983eb/alerts/q/" + zip + ".json"
+			$.ajax ({
+				dataType: 'json',
+				url: alertURL
+			}).done(function(data) {
+				if (data.alerts.length !== 0) {
+					console.log("Now there's something to work with");
+				}
+			}).fail(function(jqXHR, textStatus, errorThrown) {
+				console.log(textStatus + ": " + errorThrown);
+			});
 		},
 
-		buildForecastTable: function(tableHeader) {
-			tableHeader.forEach(function(element) {
+		/*********
+		 DATA PROCESSING
+		 *********/
+
+		createForecastArrays: function(json) {
+			const lengthOfForecast = 5;
+			let forecastDays = [];
+			let highLow = [];
+			let weatherIcon = [];
+			let conditions = [];
+			for (i = 0; i < lengthOfForecast; i++) {
+				forecastDays.push(json[i].date.weekday_short + " " + json[i].date.month + "/" + json[i].date.day);
+				highLow.push(json[i].low.fahrenheit + "\xB0 | " + json[i].high.fahrenheit + "\xB0");
+				weatherIcon.push([json[i].icon_url, json[i].icon]);
+				conditions.push(json[i].conditions);
+			}
+			weatherApp.buildForecastTable(forecastDays, highLow, weatherIcon, conditions);
+		},
+
+		/*********
+		 POPULATING DOM
+		 *********/
+
+		buildForecastTable: function(forecastDays, highLow, weatherIcon, conditions) {
+			forecastDays.forEach(function(element) {
 				$("#forecast__days").append("<th>" + element + "</th>");
 			});
+			highLow.forEach(function(element) {
+				$("#forecast__temps").append("<td>" + element + "</td>");
+			});
+			weatherIcon.forEach(function(element) {
+				$("#forecast__icon").append("<td><img src='" + element[0] + "' alt='" + element[1] + "'></td>")
+			});
+			conditions.forEach(function(element) {
+				$("#forecast__conditions").append("<td>" + element + "</td>");
+			})
 		}
 	}
 
