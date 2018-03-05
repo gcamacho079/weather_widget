2d936f577a9ed21492bd0db5e2728d51d21263d0
Lupe <lupe@acoorg.com>
2018-03-05 11:04:36 -0800

    Shorten js appending weather data to view

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,9 @@
 "use strict";
 
-var weatherApp = {
+var weatherModel = {
 
 	init: function init() {
-		weatherApp.findLocation();
+		weatherModel.findLocation();
 	},
 
 	apiCall: function apiCall(url, requestType) {
@@ -15,42 +15,42 @@ var weatherApp = {
 	},
 
 	findLocation: function findLocation() {
-		var locationPromise = weatherApp.apiCall("https://www.googleapis.com/geolocation/v1/geolocate?key=" + config.GOOGLE_KEY, "POST");
+		var locationPromise = weatherModel.apiCall("https://www.googleapis.com/geolocation/v1/geolocate?key=" + config.GOOGLE_KEY, "POST");
 
 		$.when(locationPromise).done(function (data) {
-			weatherApp.getWeatherInfo(data.location.lat + "," + data.location.lng);
+			weatherModel.getWeatherInfo(data.location.lat + "," + data.location.lng);
 		}).fail(function (jqXHR, textStatus, errorThrown) {
 			console.log("now we call the error function");
 		});
 	},
 
 	getWeatherInfo: function getWeatherInfo(latlong) {
-		var weatherPromise = weatherApp.apiCall(weatherEndpoint = "http://api.wunderground.com/api/" + config.WUNDERGROUND_KEY + "/forecast10day/q/" + latlong + ".json", "GET");
-		var advisoryPromise = weatherApp.apiCall("http://api.wunderground.com/api/" + config.WUNDERGROUND_KEY + "/alerts/q/" + latlong + ".json", "GET");
+		var weatherPromise = weatherModel.apiCall("http://api.wunderground.com/api/" + config.WUNDERGROUND_KEY + "/forecast10day/q/" + latlong + ".json", "GET");
+		var advisoryPromise = weatherModel.apiCall("http://api.wunderground.com/api/" + config.WUNDERGROUND_KEY + "/alerts/q/" + latlong + ".json", "GET");
 
 		$.when(weatherPromise, advisoryPromise).done(function (forecast, advisories) {
-			weatherApp.createForecastArrays(forecast[0].forecast.simpleforecast.forecastday);
+			weatherController.createForecastArrays(forecast[0].forecast.simpleforecast.forecastday);
 			if (advisories[0].alerts.length !== 0) {
-				weatherApp.processAlerts(advisories.alerts);
+				weatherController.processAlerts(advisories.alerts);
 			}
 		}).fail(function (jqXHR, textStatus, errorThrown) {
 			console.log(textStatus + ": " + errorThrown);
 		});
-	},
+	}
 
-	/*********
- DATA PROCESSING
- *********/
+	/*****
+ 	Controller/Data Processing **************************************************/
 
+};var weatherController = {
 	createForecastArrays: function createForecastArrays(json) {
+		var numberOfDays = 5;
 		var forecastDays = [];
 		var highLow = [];
 		var weatherIcon = [];
 		var conditions = [];
 		var rain = [];
 		var wind = [];
-		for (var i = 0; i < 5; i++) {
-			// Days in forecast
+		for (var i = 0; i < numberOfDays; i++) {
 			forecastDays.push(json[i].date.weekday_short + " " + json[i].date.month + "/" + json[i].date.day);
 			highLow.push("<span class='low-temp'>" + json[i].low.fahrenheit + "\xB0</span> | <span class='high-temp'>" + json[i].high.fahrenheit + "\xB0</span>");
 			weatherIcon.push([json[i].icon_url, json[i].icon]);
@@ -58,7 +58,7 @@ var weatherApp = {
 			rain.push(json[i].qpf_allday.in);
 			wind.push(json[i].avewind.mph);
 		}
-		weatherApp.buildForecastTable(forecastDays, highLow, weatherIcon, conditions, rain, wind);
+		weatherView.buildForecastTable(forecastDays, highLow, weatherIcon, conditions, rain, wind);
 	},
 
 	processAlerts: function processAlerts(alertData) {
@@ -66,40 +66,32 @@ var weatherApp = {
 		alertData.forEach(function (thisAlert) {
 			allAlerts.push([thisAlert.description, thisAlert.message, thisAlert.expires]);
 		});
-		weatherApp.buildAlertBox(allAlerts);
-	},
+		weatherView.buildAlertBox(allAlerts);
+	}
 
-	/*********
- POPULATING DOM
- *********/
+	/*****
+ 	Forecast View ***************************************************************/
 
+};var weatherView = {
 	buildForecastTable: function buildForecastTable(forecastDays, highLow, weatherIcon, conditions, rain, wind) {
-		forecastDays.forEach(function (element) {
+		forecastDays.forEach(function (element, index) {
 			$("#forecast__days").append("<th>" + element + "</th>");
-		});
-		highLow.forEach(function (element) {
-			$("#forecast__temps").append("<td>" + element + "</td>");
-		});
-		weatherIcon.forEach(function (element) {
-			$("#forecast__icon").append("<td><img src='" + element[0] + "' alt='" + element[1] + "'></td>");
-		});
-		conditions.forEach(function (element) {
-			$("#forecast__conditions").append("<td>" + element + "</td>");
-		});
-		rain.forEach(function (element) {
-			$("#forecast__rain").append("<td><i class='fa fa-tint raindrop-icon' aria-hidden='true'></i> " + element + "\" rain</td>");
-		}), wind.forEach(function (element) {
-			$("#forecast__wind").append("<td><i class='fa fa-leaf leaf-icon' aria-hidden='true'></i> " + element + " mph wind</td>");
+			$("#forecast__temps").append("<td>" + highLow[index] + "</td>");
+			$("#forecast__icon").append("<td><img src='" + weatherIcon[index][0] + "' alt='" + element[1] + "'></td>");
+			$("#forecast__conditions").append("<td>" + conditions[index] + "</td>");
+			$("#forecast__rain").append("<td><i class='fa fa-tint raindrop-icon' aria-hidden='true'></i> " + rain[index] + "\" rain</td>");
+			$("#forecast__wind").append("<td><i class='fa fa-leaf leaf-icon' aria-hidden='true'></i> " + wind[index] + " mph wind</td>");
 		});
 	},
 
 	buildAlertBox: function buildAlertBox(alerts) {
-		//alerts = [["Heat Advisory", "this is a message about a crazy heat wave", "7:00 AM CDT on July 07, 2012"]];
 		$(".advisory-box").toggle();
 		alerts.forEach(function (element) {
 			$("#advisory-messages").append("<p><strong>" + element[0] + "</strong>: " + element[1] + "<br><small>(Expires on " + element[2] + "</small>)</p>");
 		});
-	}
+	},
+
+	showErrorMessage: function showErrorMessage(typeOfError, message) {}
 };
 
-$(document).ready(weatherApp.init);
+$(document).ready(weatherModel.init);
--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
-const weatherApp = {
+const weatherModel = {
 
 	init: function() {
-		weatherApp.findLocation();
+		weatherModel.findLocation();
 	},
 
 	apiCall: (url, requestType) => {
@@ -12,25 +12,25 @@ const weatherApp = {
 		});
 	},
 
-	findLocation: function() {
-		var locationPromise = weatherApp.apiCall(
+	findLocation: () => {
+		var locationPromise = weatherModel.apiCall(
 			"https://www.googleapis.com/geolocation/v1/geolocate?key=" + config.GOOGLE_KEY,
 			"POST"
 		);
 
 		$.when(locationPromise).done(function(data) {
-			weatherApp.getWeatherInfo(data.location.lat + "," + data.location.lng);
+			weatherModel.getWeatherInfo(data.location.lat + "," + data.location.lng);
 		}).fail(function(jqXHR, textStatus, errorThrown) {
 			console.log("now we call the error function");
 		});
 	},
 
- 	getWeatherInfo: function(latlong) {
-		var weatherPromise = weatherApp.apiCall(
-			weatherEndpoint = "http://api.wunderground.com/api/" + config.WUNDERGROUND_KEY + "/forecast10day/q/" + latlong + ".json",
+ 	getWeatherInfo: (latlong) => {
+		var weatherPromise = weatherModel.apiCall(
+			"http://api.wunderground.com/api/" + config.WUNDERGROUND_KEY + "/forecast10day/q/" + latlong + ".json",
 			"GET"
 		);
-		var advisoryPromise = weatherApp.apiCall(
+		var advisoryPromise = weatherModel.apiCall(
 			"http://api.wunderground.com/api/" + config.WUNDERGROUND_KEY + "/alerts/q/" + latlong + ".json",
 			"GET"
 		);
@@ -39,27 +39,29 @@ const weatherApp = {
 			weatherPromise,
 			advisoryPromise
 		).done(function(forecast, advisories) {
-			weatherApp.createForecastArrays(forecast[0].forecast.simpleforecast.forecastday);
+			weatherController.createForecastArrays(forecast[0].forecast.simpleforecast.forecastday);
 			if (advisories[0].alerts.length !== 0) {
-				weatherApp.processAlerts(advisories.alerts);
+				weatherController.processAlerts(advisories.alerts);
 			}
 		}).fail(function(jqXHR, textStatus, errorThrown) {
 			console.log(textStatus + ": " + errorThrown);
 		});
-	},
+	}
+}
 
-	/*********
-	DATA PROCESSING
-	*********/
+/*****
+	Controller/Data Processing **************************************************/
 
+const weatherController = {
 	createForecastArrays: function(json) {
+		const numberOfDays = 5;
 		let forecastDays = [];
 		let highLow = [];
 		let weatherIcon = [];
 		let conditions = [];
 		let rain = [];
 		let wind = [];
-		for (var i = 0; i < 5; i++) { // Days in forecast
+		for (var i = 0; i < numberOfDays; i++) {
 			forecastDays.push(json[i].date.weekday_short + " " + json[i].date.month + "/" + json[i].date.day);
 			highLow.push("<span class='low-temp'>" + json[i].low.fahrenheit + "\xB0</span> | <span class='high-temp'>" + json[i].high.fahrenheit + "\xB0</span>");
 			weatherIcon.push([json[i].icon_url, json[i].icon]);
@@ -67,7 +69,7 @@ const weatherApp = {
 			rain.push(json[i].qpf_allday.in);
 			wind.push(json[i].avewind.mph);
 		}
-		weatherApp.buildForecastTable(forecastDays, highLow, weatherIcon, conditions, rain, wind);
+		weatherView.buildForecastTable(forecastDays, highLow, weatherIcon, conditions, rain, wind);
 	},
 
 	processAlerts: function(alertData) {
@@ -75,41 +77,35 @@ const weatherApp = {
 		alertData.forEach(function(thisAlert) {
 			allAlerts.push([thisAlert.description, thisAlert.message, thisAlert.expires]);
 		});
-		weatherApp.buildAlertBox(allAlerts);
-	},
+		weatherView.buildAlertBox(allAlerts);
+	}
+}
 
-	/*********
-	POPULATING DOM
-	*********/
+/*****
+	Forecast View ***************************************************************/
 
+const weatherView = {
 	buildForecastTable: function(forecastDays, highLow, weatherIcon, conditions, rain, wind) {
-		forecastDays.forEach(function(element) {
+		forecastDays.forEach(function(element, index) {
 			$("#forecast__days").append("<th>" + element + "</th>");
+			$("#forecast__temps").append("<td>" + highLow[index] + "</td>");
+			$("#forecast__icon").append("<td><img src='" + weatherIcon[index][0] + "' alt='" + element[1] + "'></td>");
+			$("#forecast__conditions").append("<td>" + conditions[index] + "</td>");
+			$("#forecast__rain").append("<td><i class='fa fa-tint raindrop-icon' aria-hidden='true'></i> " + rain[index] + "\" rain</td>");
+			$("#forecast__wind").append("<td><i class='fa fa-leaf leaf-icon' aria-hidden='true'></i> " + wind[index] + " mph wind</td>");
 		});
-		highLow.forEach(function(element) {
-			$("#forecast__temps").append("<td>" + element + "</td>");
-		});
-		weatherIcon.forEach(function(element) {
-			$("#forecast__icon").append("<td><img src='" + element[0] + "' alt='" + element[1] + "'></td>")
-		});
-		conditions.forEach(function(element) {
-			$("#forecast__conditions").append("<td>" + element + "</td>");
-		});
-		rain.forEach(function(element) {
-			$("#forecast__rain").append("<td><i class='fa fa-tint raindrop-icon' aria-hidden='true'></i> " + element + "\" rain</td>");
-		}),
-		wind.forEach(function(element) {
-			$("#forecast__wind").append("<td><i class='fa fa-leaf leaf-icon' aria-hidden='true'></i> " + element + " mph wind</td>");
-		})
 	},
 
 	buildAlertBox: function(alerts) {
-		//alerts = [["Heat Advisory", "this is a message about a crazy heat wave", "7:00 AM CDT on July 07, 2012"]];
 		$(".advisory-box").toggle();
 		alerts.forEach(function(element) {
 			$("#advisory-messages").append("<p><strong>" + element[0] + "</strong>: " + element[1] + "<br><small>(Expires on " + element[2] + "</small>)</p>")
 		})
+	},
+
+	showErrorMessage: function(typeOfError, message) {
+
 	}
 }
 
-$(document).ready(weatherApp.init);
+$(document).ready(weatherModel.init);
